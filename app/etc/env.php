bbd8b0a967a289b5ecd62e69db4d838f7cd9066e
cedarg <40638419+cedarg@users.noreply.github.com>
2018-07-11 15:01:52 -0400

    eeee

--- app/etc/env.php
+++ app/etc/env.php
@@ -24,7 +24,7 @@ return [
         ]
     ],
     'x-frame-options' => 'SAMEORIGIN',
-    'MAGE_MODE' => 'default',
+    'MAGE_MODE' => 'production',
     'session' => [
         'save' => 'files'
     ],
